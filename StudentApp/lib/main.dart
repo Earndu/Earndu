1622c8bfc9aeacadff0e1fa05aa13205e79bc5be
Orlando <59018852+ingkoon@users.noreply.github.com>
2021-04-12 16:34:09 +0900

    [2021-04-12] success build ver

--- StudentApp/lib/main.dart
+++ StudentApp/lib/main.dart
@@ -1,13 +1,10 @@
-//import 'dart:js';
 import 'package:flutter/material.dart';
-
 import 'src/login.dart';
 
 //플루터 시작 부분
 void main() {
   runApp(MaterialApp(
     //앱을 시작하는 함수, 여기서부터 시작된다.
-
     debugShowCheckedModeBanner: false,
     title: 'Start',
     home: Login_Page(),
